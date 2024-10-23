6eb6686c1b97f2305738a9d32fb7f38660f993d0
sudotto <codekiwi132@gmail.com>
2024-10-22 19:44:14 -0700

    learned i knew nothing about pointers, fixed file contents storage

--- src/main.c
+++ src/main.c
@@ -24,19 +24,26 @@ void error(char* msg, int code){                                                
 	exit(code);                                                                          // exit with a code
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]){
 	if(argc < 2){                                                                        // if there aren't enough arguments
 		error("missing input file", 1);                                                  // missing file error
 	}
-	FILE* file;                                                                          // file variable
+	FILE *file;                                                                          // file variable
 	if(!fopen(argv[1], "r")){                                                            // if opening file fails
 		error("cannot open file", 2);                                                    // can't open file error
 	}
 	file = fopen(argv[1], "r");                                                          // open the file
-	char contents[1000];                                                                 // variable for contents of file
+	char *contents[100];                                                                 // variable for contents of file
+	char *line = malloc(100);
 	// TODO:
 	// Redo contents to be an array of char* (char* being a line) then work on tokenize()
 	//tokenize(contents);
+	for(int i = 0; i < 100; i++){
+		fgets(line, 100, file);
+		contents[i] = malloc(100);
+		strcpy(contents[i], line);
+	}
+	printf("%s", contents[3]);
 	fclose(file);                                                                        // close the file
 	return 0;                                                                            // SUCCESS!
 }
