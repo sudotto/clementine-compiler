60ab9be1b4a3a32ac84b937b720ac1c44e41823c
sudotto <codekiwi132@gmail.com>
2024-10-26 22:00:18 -0700

    printing file contents DONE

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,41 @@
 #include <vector>
 #include <stdlib.h>
 
-typedef enum token_type {                                            // enum for tokens
-	ret,                                                             // return
-	int_lit,                                                         // int
-	semi                                                             // ;
+typedef enum token_type {                                                      // enum for tokens
+	ret,                                                                       // return
+	int_lit,                                                                   // int
+	semi                                                                       // ;
 } token_type;
 
-typedef struct token {                                               // token structure
-	token_type type;                                                 // attribute for token type
-	std::string value;                                               // attribute for value of token
+typedef struct token {                                                         // token structure
+	token_type type;                                                           // attribute for token type
+	std::string value;                                                         // attribute for value of token
 } token;
 
-void error(std::string msg, int code){                               // function to print an error
-	std::cout << "\033[1;31m[ERROR] " << msg << "\033[0m\n";         // print red error text
-	exit(code);                                                      // exit with error code
+void error(std::string msg, int code){                                         // function to print an error
+	std::cout << "\033[1;31m[ERROR] " << msg << "\033[0m" << std::endl;        // print red error text
+	exit(code);                                                                // exit with error code
 }
 
-std::vector<std::string> read_file(std::string filename){
-	std::vector<std::string> contents;
-	std::ifstream file;                                              // 
-	file.open(filename);
-	if(!file.is_open()){
-		error("couldn't open file", 2);
+std::vector<std::string> read_file(std::string filename){                      // read file func
+	std::vector<std::string> contents;                                         // contents string vector
+	std::ifstream file;                                                        // file variable
+	file.open(filename);                                                       // open provided file
+	if(!file.is_open()){                                                       // if didn't open
+		error("couldn't open file", 2);                                        // couldn't open
 	}
-	file.close();
-	return contents;
+	std::string line;
+	while(std::getline(file, line)){
+		std::cout << line << std::endl;
+	}
+	file.close();                                                              // close file
+	return contents;                                                           // ret file contents
 }
 
-int main(int argc, char* argv[]){                                    // main function
-	if(argc < 2){                                                    // if less than 2 arguments
-		error("input file missing", 1);                              // return error
+int main(int argc, char* argv[]){                                              // main function
+	if(argc < 2){                                                              // if less than 2 arguments
+		error("input file missing", 1);                                        // return error
 	}
-	std::vector<std::string> file_contents = read_file(argv[1]); 
-	return 0;
+	std::vector<std::string> file_contents = read_file(argv[1]);               // read file
+	return 0;                                                                  // success!
 }
